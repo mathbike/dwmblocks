333176dbc112b5dec3942ddf57d8138ec0e9cb4f
mathbike <mathbike8@gmail.com>
2022-03-04 04:11:59 +0000

    update

---
+++ blocks.h
@@ -0,0 +1,14 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/	
+
+	{"", 		"sb-date.sh",		60,			0},
+
+	{"",		"sb-battery.sh",	60,			0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " ";
+static unsigned int delimLen = 5;
+
